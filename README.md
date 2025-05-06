f5f39b47b99900514af5060566f908fb6083f3bc
salzoubi <sal.alzoubi123@gmail.com>
2025-05-06 08:54:17 +0300

    commit 3

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-## Saleem Alzoubi
-### Resume
+## Sawaed AlElm
+### Website
 
 
 How to deploy:
 1. ng build --configuration production --base-href "/sawaed/"
-2. npx angular-cli-ghpages --dir=dist/saleem-alzoubi-portfolio
+2. npx angular-cli-ghpages --dir=dist/sawaed-website
 3. settings -> pages
--- production.sh
+++ production.sh
@@ -3,5 +3,5 @@ ng build --configuration production
 git checkout production
 find . -maxdepth 1 -type f -not \( -name ".git" -or -name ".gitignore" -or -name "production.sh" \) -delete
 rm -rf assets
-cp -r dist/saleem-alzoubi-portfolio/* ./
-cp dist/saleem-alzoubi-portfolio/.htaccess ./.htaccess
+cp -r dist/sawaed-website/* ./
+cp dist/sawaed-website/.htaccess ./.htaccess
